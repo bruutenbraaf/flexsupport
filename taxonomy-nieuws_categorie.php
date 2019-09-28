a0a3d7bc4b57abfd603ac06e1a7755a845d4adb2
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2019-09-28 09:53:01 +0200

    Fix

--- taxonomy-nieuws_categorie.php
+++ taxonomy-nieuws_categorie.php
@@ -4,7 +4,7 @@ get_header(); ?>
 <?php
 // Define taxonomy prefix
 // Replace NULL with the name of the taxonomy eg 'category'
-$taxonomy_prefix = 'nieuws_categorie';
+$taxonomy_prefix = 'branches';
 
 // Define term ID
 // Replace NULL with ID of term to be queried eg '123' 
@@ -42,44 +42,62 @@ $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
                 <svg width="26" height="79" viewBox="0 0 26 79" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M25.5605 65.5607L12.9999 78.1213L0.439227 65.5607L2.56055 63.4393L11.4999 72.3787L11.4999 -6.14611e-07L14.4999 -4.83477e-07L14.4999 72.3787L23.4392 63.4393L25.5605 65.5607Z" fill="#001F3F" />
                 </svg>
-
             </div>
         </div>
     </div>
 </section>
 
-
-<section class="overv overv-op">
+<section class="news-arch">
     <div class="container">
         <div class="row">
             <div class="offset-md-1 col-md-10">
                 <?php if (have_posts()) : ?>
                     <div class="arch-items">
-                        <?php while (have_posts()) : the_post(); ?>
-                            <div class="arch--inner">
-                                <a href="<?php the_permalink() ?>">
-                                    <?php setup_postdata($post); ?>
-                                    <h3><?php the_title(); ?></h3>
-                                    <?php if (have_rows('opleiding_informatie')) : ?>
-                                        <?php while (have_rows('opleiding_informatie')) : the_row(); ?>
-                                            <span class="locat"><?php the_sub_field('locatie'); ?></span>
-                                            <p><?php the_sub_field('korte_omschrijving'); ?></p>
-                                        <?php endwhile; ?>
-                                    <?php endif; ?>
-                                </a>
-                                <a class="moreinfo" href="<?php the_permalink(); ?>"><?php _e('Lees meer', 'flexsupport'); ?>
-                                    <svg width="55" height="27" viewBox="0 0 55 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M42.0607 0.939331L54.6213 13.5L42.0607 26.0606L39.9393 23.9393L48.8787 15L0 15L0 12L48.8787 12L39.9393 3.06065L42.0607 0.939331Z" />
-                                    </svg>
-                                </a>
-                            </div>
-                        <?php endwhile; ?>
+                        <div class="row">
+                            <?php while (have_posts()) : the_post(); ?>
+                                <div class="col-md-6 arch-item">
+                                    <a href="<?php the_permalink() ?>">
+                                        <div class="thumb-crop">
+                                            <div class="thumb" style="background-image:url(<?php echo get_the_post_thumbnail_url($post, 'large'); ?>);">
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a href="<?php the_permalink() ?>">
+                                        <?php setup_postdata($post); ?>
+                                        <h3><?php the_title(); ?></h3>
+                                        <p>
+                                            <?php $theterms = wp_get_post_terms($post->ID, 'nieuws_categorie', array("fields" => "names")); ?>
+                                            <?php foreach ($theterms as $theterm) { ?>
+                                                <span class="tax-name"><?php echo $theterm; ?> | </span>
+                                            <?php } ?>
+                                            <?php the_time('d-m-Y'); ?>
+                                        </p>
+                                    </a>
+                                    <a href="<?php the_permalink(); ?>" class="btn"><?php _e('Lees bericht', 'flexsupport'); ?></a>
+                                </div>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?>
+                        </div>
                     </div>
                 <?php endif; ?>
             </div>
-            <?php wp_reset_postdata(); ?>
         </div>
     </div>
 </section>
+
+<?php if (paginate_links()) { ?>
+    <section class="pagenumbers">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <?php echo paginate_links(array(
+                            'prev_text' => '',
+                            'next_text' => ''
+                        )); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
 
 <?php get_footer(); ?>
