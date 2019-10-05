1a89c81b5967264b435c80c565e6872f0a8571e5
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-05 17:16:21 +0200

    Form

---
+++ templates/sitemap.php
@@ -0,0 +1,42 @@
+<?php
+// Template name: Sitemap
+get_header(); ?>
+<?php if (have_rows('header')) : ?>
+    <?php while (have_rows('header')) : the_row(); ?>
+        <section class="header--sngl">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 offset-md-1">
+                        <?php the_sub_field('titel'); ?>
+                    </div>
+                    <div class="offset-md-1 col-md-6">
+                        <?php the_sub_field('intro'); ?>
+                    </div>
+                    <div class="col-md-5 justify-content-end align-items-end d-flex">
+                        <div>
+                            <?php $knop = get_sub_field('knop'); ?>
+                            <?php if ($knop) { ?>
+                                <a class="small" href="<?php echo $knop['url']; ?>" target="<?php echo $knop['target']; ?>"><?php echo $knop['title']; ?>
+                                    <svg width="79" height="27" viewBox="0 0 79 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M65.5607 0.93932L78.1213 13.5L65.5607 26.0606L63.4393 23.9393L72.3787 15L2.45844e-06 15L1.93391e-06 12L72.3787 12L63.4393 3.06064L65.5607 0.93932Z" fill="#35FFB6" />
+                                    </svg>
+                                </a>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<section class="kandi">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <?php echo do_shortcode('[sitemap]');?>
+            </div>
+        </div>
+</section>
+
+<?php get_footer(); ?>
