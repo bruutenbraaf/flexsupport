c339558c2141d1467b2ea14c2855d58ddde393dd
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-08 21:54:23 +0200

    tools

---
+++ single-tools.php
@@ -0,0 +1,9 @@
+<?php
+get_header(); ?>
+<?php get_template_part('template-parts/content', 'single'); ?>
+
+
+<?php get_template_part('template-parts/content', 'related'); ?>
+
+
+<?php get_footer(); ?>
