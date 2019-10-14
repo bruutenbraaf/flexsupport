fd80ec8e7d4579bfffffbffa2f04bdc6b3dfd5bf
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-14 21:13:43 +0200

    Copyright

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <span>&copy; Flexsupport <?php echo date("Y"); ?></span>
+                    <span>&copy; Flexsupport 2007 - <?php echo date("Y"); ?></span>
                 </div>
                 <div class="col-md-8 d-flex justify-content-end">
                     <?php wp_nav_menu(array('theme_location' => 'socket_menu')); ?>
