35208f2804fea7ddf7dbbb1a28bdaea1b2275648
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-20 11:01:20 +0200

    Animation fix

--- js/custom.js
+++ js/custom.js
@@ -36,13 +36,13 @@ var scene = new ScrollMagic.Scene({ triggerElement: ".marq", duration: 300 })
   .addTo(controller);
 
 
-var tween = TweenMax.to(".tp", 1, { className: "+=scr--right" });
+var tween = TweenMax.to(".tpx", 1, { className: "+=scr--rightx" });
 var scene = new ScrollMagic.Scene({ triggerElement: ".marq--vac", duration: 300 })
   .setTween(tween)
   .addIndicators()
   .addTo(controller);
 
-var tween = TweenMax.to(".bt", 1, { className: "+=scr--left" });
+var tween = TweenMax.to(".btx", 1, { className: "+=scr--leftx" });
 var scene = new ScrollMagic.Scene({ triggerElement: ".marq--vac", duration: 300 })
   .setTween(tween)
   .addIndicators()
