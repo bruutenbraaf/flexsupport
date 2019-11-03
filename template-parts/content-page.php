9b741e7f99a0c2d5a293e272923a1e0cdb05ba5d
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-03 09:39:15 +0100

    Team fix

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -432,7 +432,7 @@
                         <?php if ($knop) { ?>
                             <div class="col-md-10 offset-md-1">
                                 <div class="row">
-                                    <a href="<?php echo $knop['url']; ?>" <?php if ($knop['target']) { ?>target="<?php echo $knop['target']; ?>" <?php } ?>><?php echo $knop['title']; ?></a>
+                                    <a class="btn" href="<?php echo $knop['url']; ?>" <?php if ($knop['target']) { ?>target="<?php echo $knop['target']; ?>" <?php } ?>><?php echo $knop['title']; ?></a>
                                 </div>
                             </div>
                         <?php } ?>
