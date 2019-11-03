2ea14579f3020fd93e054ebe570fc6f9d0fc0e1a
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-03 11:26:57 +0100

    Sitemap

--- templates/sitemap.php
+++ templates/sitemap.php
@@ -30,11 +30,46 @@ get_header(); ?>
     <?php endwhile; ?>
 <?php endif; ?>
 
-<section class="kandi">
+<section class="site">
     <div class="container">
         <div class="row">
-            <div class="col-md-10 offset-md-1">
-                <?php echo do_shortcode('[sitemap]');?>
+            <div class="col-md-5 offset-md-1">
+                <h2 id="pages"><?php _e("Pagina's", "flexsupport"); ?></h2>
+                <ul>
+                    <?php
+                    // Add pages you'd like to exclude in the exclude here
+                    wp_list_pages(
+                        array(
+                            'exclude' => '352',
+                            'title_li' => '',
+                        )
+                    );
+                    ?>
+                </ul>
+            </div>
+            <div class="col-md-5">
+                <h2 id="pages"><?php _e("Regio's", "flexsupport"); ?></h2>
+                <?php
+                $terms = get_terms('regio');
+
+                echo '<ul>';
+
+                foreach ($terms as $term) {
+
+                    // The $term is an object, so we don't need to specify the $taxonomy.
+                    $term_link = get_term_link($term);
+
+                    // If there was an error, continue to the next term.
+                    if (is_wp_error($term_link)) {
+                        continue;
+                    }
+
+                    // We successfully got a link. Print it out.
+                    echo '<li><a href="' . esc_url($term_link) . '">' . $term->name . '</a></li>';
+                }
+
+                echo '</ul>'; ?>
+                <?php wp_reset_postdata(); ?>
             </div>
         </div>
 </section>
