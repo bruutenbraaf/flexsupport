98e9ec8a25f24b0b17f95b25a040f56cba5f264a
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-03 07:54:11 +0100

    Share buttons

--- single-kennisbank.php
+++ single-kennisbank.php
@@ -2,7 +2,7 @@
 get_header(); ?>
 <?php get_template_part('template-parts/content', 'single'); ?>
 
-<div class="container">
+<div class="container share">
     <div class="row">
         <div class="col-md-10 offset-md-1">
             <?php echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_612e"]'); ?>
