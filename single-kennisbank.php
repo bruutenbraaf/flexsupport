4ad3c7c707428819cfcbd5c4e6b94e6ed61ad2c8
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-03 07:51:18 +0100

    AddThis

--- single-kennisbank.php
+++ single-kennisbank.php
@@ -2,6 +2,8 @@
 get_header(); ?>
 <?php get_template_part('template-parts/content', 'single'); ?>
 
+<?php echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_612e"]');?>
+
 <?php get_template_part('template-parts/content', 'related'); ?>
 
 <?php get_footer(); ?>
