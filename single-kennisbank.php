a91960829407360218c2d1a116ea31c13390ac43
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-03 19:28:46 +0100

    FIx

--- single-kennisbank.php
+++ single-kennisbank.php
@@ -2,13 +2,14 @@
 get_header(); ?>
 <?php get_template_part('template-parts/content', 'single'); ?>
 
+<!-- 
 <div class="container share">
     <div class="row">
         <div class="col-md-10 offset-md-1">
-            <?php echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_612e"]'); ?>
+            <?php // echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_612e"]'); ?>
         </div>
     </div>
-</div>
+</div> -->
 
 <?php get_template_part('template-parts/content', 'related'); ?>
 
