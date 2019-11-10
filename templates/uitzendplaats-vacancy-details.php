fb80dfabbf82b15ff982d98fc7068df8f10393b8
Bruut & Braaf <info@bruutenbraaf.nl>
2019-11-10 20:25:08 +0100

    fixes

--- templates/uitzendplaats-vacancy-details.php
+++ templates/uitzendplaats-vacancy-details.php
@@ -223,14 +223,6 @@
 				</div>
 			</div>
 		<?php } ?>
-		<script>
-			jQuery('p.description').readmore({
-				speed: 75,
-				collapsedHeight: 155,
-				moreLink: '<a class="readmore" href="#">Lees meer</a>',
-				lessLink: '<a class="readmore" href="#">Verbergen</a>'
-			});
-		</script>
 		<?php if (!$view_data && property_exists($view_data, 'data')) { ?>
 			<div class="container app-form" id="apply">
 				<div class="row">
